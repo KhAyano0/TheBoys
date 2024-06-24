b45de3d8e91529ef69133fced7fbb78fbd624665
DossantosChristophe <tof.ds973@gmail.com>
2024-06-24 16:57:48 -0300

    Update of models,views and admin, creation of serializers

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Character(models.Model):
+    character_id = models.AutoField(primary_key=True)
+    character_fullname = models.CharField(max_length=100, default='')
+    character_nickname = models.CharField(max_length=100, default='')
+    character_age = models.IntegerField(default=0)
+    character_group = models.CharField(max_length=100, default='')
+    character_description = models.CharField(max_length=255, default='')
+    character_image = models.CharField(max_length=255,default='')
+
+    def __str__(self):
+        return f'Fullname: {self.character_fullname} | Nickname: {self.character_nickname} | Age: {self.character_age} | Group: {self.character_group} | Description: {self.character_description}'
+    
--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Character
+from .serializers import CharacterSerializer
+
+import json
 # Create your views here.
+@api_view(['GET'])
+def get_characters(request):
+    if request.method == 'GET':
+        characters = Character.objects.all()
+
+        serializer = CharacterSerializer(characters, many=True)
+
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
