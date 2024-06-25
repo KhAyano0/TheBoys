34c7c7899b0686a4b240da5cfe3849fc8a51f093
MAIZEROI Gerson <maizeroi.gerson@gmail.com>
2024-06-25 11:36:13 -0300

    Footer et main

--- mon-app/src/App.svelte
+++ mon-app/src/App.svelte
@@ -1,4 +1,4 @@
-<nav class="flex items-center justify-between flex-wrap bg-[#AE1C1C] p-0">
+<body class = "bg-[#1D256D]"><header> <nav class="flex items-center justify-between flex-wrap bg-[#AE1C1C] p-0">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
   </div>
   <div class="block lg:hidden">
@@ -12,13 +12,19 @@
   </div>
   <button type="submit" class="m-0 rounded-md bg-[#C1850F] px-16 py-2 text-white"> Rechercher
 </button>
-  <div class="text-sm lg:flex-grow">
-    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+  <div class="text-sm flex-grow ml-64 lg:flex lg:items-center lg:w-auto inset-y-0 right-0">
+    <a href="#responsive-header" class="block mt-4  lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
       Guest or Admin
     </a>
   </div>
 </nav>
+</header> 
 
-<main>
-<h1>Ceci est un titre</h1>
+
+<main class = "m-64">
+<h1>Ceci est un titre dans un main</h1>
 </main>
+
+
+<footer class = "bg-black text-white pb-4">Copyright dans un footer</footer>
+</body>
